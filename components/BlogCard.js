84e670d2e15d6763092930c40576397be6584bcd
Vukichinuky <gudraliin@gmail.com>
2023-03-26 20:15:53 +0200

    asd

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -3,20 +3,24 @@ import moment from "moment";
 import Link from "next/link";
 import Image from "next/image";
 
-function BlogPost({ title, author, coverPhoto, datepublish, slug, content}) {
+function BlogPost({ title, author, coverPhoto, datepublish, slug, content }) {
   return (
-    <div className=" hover:opacity-70 ">
-        <div className="    overflow-hidden hover:animate-pulse    w-72 bg-orange-600  sm:w-96 shadow-xl rounded-3xl m-1 ">
-          <Link href={`/posts/`+ slug}>
+    <div className=" ">
+      <div className=" p-10    overflow-hidden hover:animate-pulse  shadow-black shadow-lg  w-42 bg-[#FFBC42]  sm:w-42 shadow-xl rounded-3xl m-1 ">
+        <div className="">
+          <Link href={`/posts/` + slug}>
             <div className={styles.imgContainer}>
-              <Image layout="fill" src={coverPhoto.url} alt="ccvb" />
+              <Image layout="fill" src={coverPhoto.url} className={'rounded-3xl shadow-black shadow-lg'} alt="ccvb" />
             </div>
           </Link>
-          <div className=' text-center my-6 text-xl'>
+        </div>
+        <div className=' text-center my-6 text-xl'>
+          <div className=" bg-[#fea400] z-10 relative  rounded-3xl px-5 py-14 shadow-black shadow-lg">
             <h2>{title}</h2>
+          </div>
+          <div className="bg-[#FFD892] -z-10 rounded-3xl px-5 py-10 shadow-black shadow-2xl ">
             <div className={styles.details}>
               <div className={styles.author}>
-                
                 <img src={author.avatar.url} alt={author.ime} />
                 <h3>{author.name}</h3>
               </div>
@@ -26,6 +30,7 @@ function BlogPost({ title, author, coverPhoto, datepublish, slug, content}) {
             </div>
           </div>
         </div>
+      </div>
     </div>
   );
 }
