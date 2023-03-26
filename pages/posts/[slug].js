b136ae3889c2c93a2e21b90dfa63edcbeef89d61
Vukichinuky <gudraliin@gmail.com>
2023-03-26 21:06:53 +0200

    600secundes revalidate

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -5,9 +5,10 @@ import Navbar from '/components/Navbar.jsx'
 import Header from '/components/header.js'
 import Footer from '/components/footer.js'
 
+export const revalidate = 600;
 
 const graphcms = new GraphQLClient(
-    "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cld1yz5560k8j01umg34xfwv5/master");
+  "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cld1yz5560k8j01umg34xfwv5/master");
 
 const QUERY = gql`
   query Post($slug: String!) {
@@ -65,44 +66,44 @@ export default function BlogPost({ post }) {
   return (
     <main className={styles.blog}>
 
-<nav>
-<Header />
+      <nav>
+        <Header />
 
-    <Navbar />
-</nav>
+        <Navbar />
+      </nav>
 
-   <div className="sm:flex sm:justify-center  max-w-6xl m-auto sm:items-center sm:flex-col ">
-             <div className="sm:flex sm:flex-col  sm:justify-evenly sm:mt-20">
-                 <img
-                        className=' m-auto sm:rounded-2xl sm:shadow-2xl '
-                        src={post.coverPhoto.url}
-                        alt={post.title}
-                 />
-                 <div className=' p-10 '>
-                        <div className={styles.authdetails}>
-                          <img src={post.author.avatar.url} alt={post.author.ime} />
-                          <div className=' '>
-                            <h6>By {post.author.ime} </h6>
-                            <h6 className={styles.date}>
-                 {moment(post.datepublish).format("MMMM d, YYYY")}
-                            </h6>
-             </div>
-         </div>
-       </div>
-       
-             </div>
-             <div><h2 className="text-center text-xl   max-w-6xl m-auto sm:text-3xl">{post.title}</h2></div>
+      <div className="sm:flex sm:justify-center  max-w-6xl m-auto sm:items-center sm:flex-col ">
+        <div className="sm:flex sm:flex-col  sm:justify-evenly sm:mt-20">
+          <img
+            className=' m-auto sm:rounded-2xl sm:shadow-2xl '
+            src={post.coverPhoto.url}
+            alt={post.title}
+          />
+          <div className=' p-10 '>
+            <div className={styles.authdetails}>
+              <img src={post.author.avatar.url} alt={post.author.ime} />
+              <div className=' '>
+                <h6>By {post.author.ime} </h6>
+                <h6 className={styles.date}>
+                  {moment(post.datepublish).format("MMMM d, YYYY")}
+                </h6>
+              </div>
+            </div>
+          </div>
 
-             <div
-             
-       className='  sm:m-28 p-8  sm:text-xl text-m'
-       dangerouslySetInnerHTML={{ __html: post.content.html }}
-             ></div>
-   </div>
-   <section> 
+        </div>
+        <div><h2 className="text-center text-xl   max-w-6xl m-auto sm:text-3xl">{post.title}</h2></div>
 
-<Footer />
-</section>
+        <div
+
+          className='  sm:m-28 p-8  sm:text-xl text-m'
+          dangerouslySetInnerHTML={{ __html: post.content.html }}
+        ></div>
+      </div>
+      <section>
+
+        <Footer />
+      </section>
     </main>
   );
 }
