b22b91b7b2e663fe12319eddf6e25d1f9d8889b7
Drago Vukovic <gudraliin@gmail.com>
2023-11-18 19:13:28 +0100

    hi

--- pages/galerija.js
+++ pages/galerija.js
@@ -1,63 +1,90 @@
-'use client'
-import NavVert from '/components/NavVert.jsx'
-import Header from '/components/header.js'
-import Footer from '/components/footer.js'
-import Head from 'next/head'
-import { Swiper, SwiperSlide, Navigation, Autoplay } from 'swiper/react';
-import 'swiper/css';
+"use client";
+import NavVert from "/components/NavVert.jsx";
+import Header from "/components/header.js";
+import Footer from "/components/footer.js";
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Galerija() {
-    return (
-        <div>
-            <Head>
+  return (
+    <div>
+      <Head></Head>
+      <main>
+        <section className=" ">
+          <Header />
+          <NavVert />
+        </section>
+        <section>
+          <div className="">
+            <h1 className="text-[#FEA400] text-center text-4xl font-bold m-8">
+              {" "}
+              Галерија
+            </h1>
+            <div className="  mt-28 text-justify p-8  max-w-6xl m-auto   z-10 ">
+              <h1 className=" mb-10 font-bold text-white bg-[#f37748] py-4 rounded-xl px-3  text-left sm:text-2xl">
+                Новогодишње чаролије - 25.11.2021 - 01.01.2022
+              </h1>
+              <div className=" md:flex flex-wrap   ">
+                <Image
+                  width={400}
+                  height={600}
+                  className="m-2 h-96"
+                  src="/325591657_3371335779799594_8455277550985690747_n.jpg"
+                  alt=""
+                />
 
-            </Head>
-            <main>
+                <img
+                  className="m-2 h-96"
+                  src="/326795598_915356876138603_4396425158505957261_n.jpg"
+                  alt=""
+                />
 
-                <section className=' '>
-                    <Header />
-                    <NavVert />
+                <img
+                  className="m-2 h-96"
+                  src="/326802203_714838903358346_6275484961395910159_n.jpg"
+                  alt=""
+                />
 
-                </section>
-                <section>
+                <img
+                  className="m-2 h-96"
+                  src="/328320934_516951200525055_3775321121430441807_n.jpg"
+                  alt=""
+                />
 
-                    <div className=''>
+                <img
+                  className="m-2 h-96"
+                  src="/328468034_1771102369957166_9032641668976728861_n.jpg"
+                  alt=""
+                />
+              </div>
 
-                        <div className='  mt-28 text-justify p-8  max-w-6xl m-auto   z-10 '>
-
-                            <h1 className=' mb-10 font-bold text-white bg-yellow-600  text-center sm:text-2xl'>Поводом школске славе Светог Саве ученици и учитељи ПО Брањево су у част првог српског просветитеља извели рецитације и пјевали химну Светом Сави.</h1>
-                            <div className=' md:flex flex-wrap   '>
-                                <Swiper
-                                    modules={Navigation}
-                                    spaceBetween={20}
-                                    slidesPerView={3}
-                                    onSlideChange={() => console.log('slide change')}
-                                    onSwiper={(swiper) => console.log(swiper)} >
-                                    <SwiperSlide><img className='m-2 h-96' src="/325591657_3371335779799594_8455277550985690747_n.jpg" alt="" /></SwiperSlide>
-                                    <SwiperSlide><img className='m-2 h-96' src="/326795598_915356876138603_4396425158505957261_n.jpg" alt="" /></SwiperSlide>
-                                    <SwiperSlide><img className='m-2 h-96' src="/326802203_714838903358346_6275484961395910159_n.jpg" alt="" /></SwiperSlide>
-                                    <SwiperSlide><img className='m-2 h-96' src="/328320934_516951200525055_3775321121430441807_n.jpg" alt="" /></SwiperSlide>
-                                    <SwiperSlide><img className='m-2 h-96' src="/328468034_1771102369957166_9032641668976728861_n.jpg" alt="" /></SwiperSlide>
-                                </Swiper>
-                            </div>
-
-                            <h1 className=' mb-10 font-bold text-white bg-yellow-600  text-center sm:text-2xl'>У ПО Кисељак реализована је радионица за родитеље на тему "Заједничко рјешавање проблема". </h1>
-                            <div className=' md:flex flex-wrap   '>
-                                <img className='m-2 h-96' src="/325860251_1134331167281611_6600092385945959450_n.jpg" alt="" />
-                                <img className='m-2 h-96' src="/327590849_948292409913811_7235699669446097449_n.jpg" alt="" />
-                                <img className='m-2 h-96' src="/327728311_842946900102455_6709622399947185957_n.jpg" alt="" />
-                            </div>
-
-
-                        </div>
-
-
-                    </div>
-                </section>
-                <section>
-                    <Footer />
-                </section>
-            </main>
-        </div >
-    )
+              <h1 className=" mb-10 font-bold mt-10 text-white bg-[#f37748] py-4 rounded-xl px-3  text-left sm:text-2xl">
+                Мала матура - 03.06.2021
+              </h1>
+              <div className=" md:flex flex-wrap   ">
+                <img
+                  className="m-2 h-96"
+                  src="/325860251_1134331167281611_6600092385945959450_n.jpg"
+                  alt=""
+                />
+                <img
+                  className="m-2 h-96"
+                  src="/327590849_948292409913811_7235699669446097449_n.jpg"
+                  alt=""
+                />
+                <img
+                  className="m-2 h-96"
+                  src="/327728311_842946900102455_6709622399947185957_n.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <Footer />
+        </section>
+      </main>
+    </div>
+  );
 }
